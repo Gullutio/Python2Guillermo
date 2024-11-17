6cd602acb686d4d14d67c737d890fec719557e78
Gullutio <guille.cv.minecraft@gmail.com>
2024-11-17 09:52:29 +0800

    fixed 158

--- ex158.py
+++ ex158.py
@@ -2,21 +2,19 @@
 # - needs to read the name of output file as well.
 # - need to output to file, not print
 # - fix the code to fulfill the requirementss
+import sys
 
-input_text = input('type in name of input file - ')
-final = ''
-with open(input_text,'r') as file:
-    lines = file.readlines()
-    for line in lines:
-        if '#' in line:
-            count = 0
-            for char in line:
-                count += 1
-                #print(char, count)
-                if char == '#':
-                    final = line[:count]
-                    final = final.replace('#','')
-                    print(final)
-        else:
-            print(line)
-        
+try:
+    final = ''
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    with open(input_file,'r') as infile:
+        with open(output_file,'w') as outfile:
+            for line in infile:
+                clean = line.split('#')[0].rstrip()
+                outfile.write(clean+'\n')
+
+    print(f'the name of your new file - {output_file}')
+
+except:
+    print('oops there was an error')
